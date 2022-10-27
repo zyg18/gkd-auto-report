78303683da1f8ca6225f21bbc3703f55bbb0277e
Lysithea <52808607+zxysbsbzxy@users.noreply.github.com>
2022-10-27 14:10:22 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import requests
 import time
 import os
+from dateutil import rrule
+import datetime
+from dateutil.parser import parse
 
 s = requests.Session()
 
@@ -31,13 +34,17 @@ def login(s: requests.Session):
 
 
 def submit(s: requests.Session):
+    days = rrule.rrule(rrule.DAILY,dtstart=parse('2022-10-27'),until=datetime.date.today()).count()
+    
+    pcr = 1 if(days%3==0) else 0
+    PCR = str(pcr)
     new_daily = {
         "number": GKD_NUMBER,
         "realname": GKD_NAME,
 
         # submitted date
         "date": time.strftime(r"%Y-%m-%d", time_now),
-        "jzdz": "北京市怀柔区中国科学院大学雁栖湖校区西区一公寓",     # Residential Address
+        "jzdz": "北京市怀柔区中国科学院大学雁栖湖校区",     # Residential Address
         "zrzsdd": "1",                       # Yesterday place to stay    1.雁栖湖  8.京外
         # Whether you are in school or not  1.是, 主要是在雁栖湖校区   5.否
         "sfzx": "1",
@@ -67,19 +74,19 @@ def submit(s: requests.Session):
         "dqqk2": "1",                        # current situation      1.无异常
         "dqqk2qt": "",
         # 昨天是否接受核酸检测
-        "sfjshsjc": "1",                     # PCR test?       1.是 0.否
+        "sfjshsjc": PCR,                     # PCR test?       1.是 0.否
         # 第一针接种
-        "dyzymjzqk": "3",                    # first vaccination situation  3.已接种
-        "dyzjzsj": "2021-03-07",             # date of first vaccination
+        "dyzymjzqk": "2",                    # first vaccination situation  3.已接种
+        "dyzjzsj": "2021-03-13",             # date of first vaccination
         "dyzwjzyy": "",
         # 第二针接种
-        "dezymjzqk": "3",                    # second vaccination situation  3.已接种
-        "dezjzsj": "2021-03-28",             # date of second vaccination
+        "dezymjzqk": "2",                    # second vaccination situation  3.已接种
+        "dezjzsj": "2021-04-03",             # date of second vaccination
         "dezwjzyy": "",
         # 第三针接种
-        "dszymjzqk": "6",                    # third vaccination situation  6.未接种
-        "dszjzsj": "2000-01-01",             # default time
-        "dszwjzyy": "在吃其他药物",            # reason of non-vaccination
+        "dszymjzqk": "2",                    # third vaccination situation  6.未接种
+        "dszjzsj": "2021-11-06",             # default time
+        "dszwjzyy": "",            # reason of non-vaccination
 
         "gtshryjkzk": "1",                   # health situation
         "extinfo": "",                       # other information
